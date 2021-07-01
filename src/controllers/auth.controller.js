982be8769030d505ca15473e43a9243fed793965
Oleksandra Talalaieva <oleksandra.talalaieva@tum.de>
2021-07-01 18:55:35 +0200

    check if user exists in db

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,19 +26,16 @@ const login = async (req, res) => {
         }).exec();
 
         // check if the password is valid
-        const isPasswordValid = bcrypt.compareSync(
-            req.body.password,
-            user.password
-        );
-        if (!isPasswordValid) return res.status(401).send({ token: null });
-
+        const isPasswordValid = bcrypt.compareSync(req.body.password, user.password)
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                token: null,
+            })
+        }
         // if user is found and password is valid
         // create a token
-        const token = jsonwebtoken.sign(
-            { _id: user._id, username: user.username, role: user.role },
-            JwtSecret,
-            {
-                expiresIn: 86400, // expires in 24 hours
+        const token = jsonwebtoken.sign({ _id: user._id, username: user.username, role: user.role }, JwtSecret, {
+            expiresIn: 86400, // expires in 24 hours
             }
         );
 
@@ -48,10 +45,10 @@ const login = async (req, res) => {
     } catch (err) {
         return res.status(404).json({
             error: "User Not Found",
-            message: err.message,
+            message: "A user with this username doesn't exist."
         });
     }
-};
+}
 
 const register = async (req, res) => {
     // check if the body of the request contains all necessary properties
@@ -72,7 +69,7 @@ const register = async (req, res) => {
         // hash the password before storing it in the database
         const salt = bcrypt.genSaltSync(8);
         const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-        
+
         // create a user object
         const userData = {
             username: req.body.username,
@@ -98,21 +95,21 @@ const register = async (req, res) => {
             {
                 expiresIn: 86400, // expires in 24 hours
             }
-        );
+        )
 
         var transporter = nodemailer.createTransport({
             service: 'Gmail', 
             auth: {
-                user: "breenderseba@gmail.com", 
+                user: "breenderseba@gmail.com",
                 pass: "breenderTeamSEBA2021" 
             }
         });
         //send an email with verification link containing email and token
                 var mailOptions = { from: 'breenderseba@gmail.com', to: retUser.email, subject: 'Account Verification Link', text: 'Hello '+ retUser.username +',\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + 'localhost:3000' + '\/confirmation\/' + retUser.email + '\/' + token + '\n\nThank You!\n' };
-                transporter.sendMail(mailOptions, function (err) {
-                    if (err) { 
+        transporter.sendMail(mailOptions, function (err) {
+            if (err) {
                         return res.status(500).send({msg: err});
-                     }
+            }
                     return res.status(200).send('A verification email has been sent to ' + retUser.email + '. It will be expire after one day. If you not get verification Email click on resend token.');
                 });
 
@@ -134,50 +131,74 @@ const register = async (req, res) => {
             });
         }
     }
-};
+}
+
+const checkIfUserExists = async (req, res) => {
+    // get the user form the database
+    try {
+        let userWithUsername = await User.findOne({
+            username: req.params.username,
+        }).exec();
+        if (userWithUsername)
+            return res.status(400).json({
+                error: { type: "username", message: "A user with this username already exists" },
+            });
+        let userWithEmail = await User.findOne({
+            email: req.params.email,
+        }).exec();
+        if (userWithEmail)
+            return res.status(400).json({
+                error: { type: "email", message: "A user with this email already exists" },
+            });
+
+        return res.status(200).json("OK");
+    } catch (err) {
+        return res.status(500).json({
+            error: "Internal server error",
+            message: err.message,
+        })
+    }
+}
 
 const confirmEmail = async (req, res) => {
-    const decodedToken = jsonwebtoken.decode(req.params.token, {complete: true}); //decode token 
-    const _id = decodedToken.payload._id; 
-    //extract id from token 
+    const decodedToken = jsonwebtoken.decode(req.params.token, { complete: true }) //decode token
+    const _id = decodedToken.payload._id
+    //extract id from token
     User.findOne({ _id: _id }, function (err, token) {
-        // token is not found into database i.e. token may have expired 
-        if (!token){
+        // token is not found into database i.e. token may have expired
+        if (!token) {
             console.log("no token")
-            return res.status(400).json({error: 'Link Expired', message:'Your verification link may have expired.'});
+            return res.status(400).json({ error: "Link Expired", message: "Your verification link may have expired." })
         }
-        // if token is found then check valid user 
-        else{
+        // if token is found then check valid user
+        else {
             User.findOne({ _id: _id, email: req.params.email }, function (err, user) {
                 // not valid user
-                if (!user){
-                    return res.status(401).json({error: 'User Not Found', message:'We were unable to find an account for this verification. Please SignUp!'});
-                } 
+                if (!user) {
+                    return res.status(401).json({ error: "User Not Found", message: "We were unable to find an account for this verification. Please SignUp!" })
+                }
                 // user is already verified
-                else if (user.isVerified){
-        
-                    return res.status(200).json({message: 'Your account has been already verified'});
-                    
+                else if (user.isVerified) {
+                    return res.status(200).json({ message: "Your account has been already verified" })
                 }
                 // verify user
-                else{
+                else {
                     console.log(user)
-                    user.isVerified = true;
+                    user.isVerified = true
                     user.save(function (err) {
-                        if(err){
-                            return res.status(500).json({message: err.message});
+                        if (err) {
+                            return res.status(500).json({ message: err.message })
                         }
                         // account successfully verified
-                        else{
-                          return res.status(200).json({message: 'Your account has been successfully verified'});
+                        else {
+                            return res.status(200).json({ message: "Your account has been successfully verified" })
                         }
-                    });
+                    })
                 }
-            });
+            })
         }
-        
-    });
-};
+    })
+}
 
 const me = async (req, res) => {
     try {
@@ -188,25 +209,19 @@ const me = async (req, res) => {
             return res.status(404).json({
                 error: "Not Found",
                 message: `User not found`,
-            });
+            })
 
-        return res.status(200).json(user);
+        return res.status(200).json(user)
     } catch (err) {
         return res.status(500).json({
             error: "Internal Server Error",
             message: err.message,
-        });
+        })
     }
-};
+}
 
 const logout = (req, res) => {
-    res.status(200).send({ token: null });
-};
+    res.status(200).send({ token: null })
+}
 
-export {
-    login,
-    register,
-    logout,
-    me,
-    confirmEmail
-};
+export { login, register, checkIfUserExists, logout, me, confirmEmail }
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,10 +1,11 @@
 import * as express from "express";
 import { checkAuthentication } from "../middlewares/auth.middleware.js";
-import {login, register, me, logout, confirmEmail} from "../controllers/auth.controller.js";
+import {login, register,checkIfUserExists, me, logout, confirmEmail} from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/login", login); //login
+authRouter.get("/userExists/:email/:username", checkIfUserExists);
 authRouter.post("/register", register); // register a new user
 authRouter.get("/me", checkAuthentication, me); // get own username, requires a logged in user
 authRouter.get("/logout", checkAuthentication, logout); // logout user
